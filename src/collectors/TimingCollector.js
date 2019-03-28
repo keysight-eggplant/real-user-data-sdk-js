134dc76f36ee041b0254283f423a930c5bac3fc9
MoHo Khaleqi <habibsandyfan@gmail.com>
2019-03-28 17:00:08 +0000

    [RUM-18631][RUM-18705][FEATURE] working on timing collector

--- src/collectors/TimingCollector.js
+++ src/collectors/TimingCollector.js
@@ -6,52 +6,10 @@ export default class TimingCollector {
     this.performanceData = window.performance.timing;
   }
 
-   eventStart() {
-    this.eventStart = this.performanceData.navigationStart;
-  }
-
-   eventEnd() {
-    this.eventEnd = this.performanceData.loadEventEnd || this.performanceData.domContentLoadedEventEnd;
-  }
-
   async prepare (event) {
-    event.eventStart = this.eventStart;
-    event.eventEnd = this.eventEnd;
-
+    event.eventStart = this.performanceData.navigationStart;
+    event.eventEnd = this.performanceData.domContentLoadedEventEnd;
     return event;
   }
 
 }
-
-// https://webmasters.stackexchange.com/questions/87764/javascript-page-load-time-calculation-returns-negative
-// https://stackoverflow.com/questions/7606972/measuring-site-load-times-via-performance-api
-/*
-$(window).load(function(){
-  setTimeout(function(){
-  window.performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {};
-  var timing = performance.timing || {};
-  var parseTime = timing.loadEventEnd - timing.responseEnd;
-  console.log('Parsetime: ', parseTime);
-  }, 0);
- });
-
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
-//Calculate the total page load time
-var perfData = window.performance.timing;
-var pageLoadTime = perfData.loadEventEnd - perfData.navigationStart;
-
-//Calculate request response time
-var connectTime = perfData.responseEnd - perfData.requestStart;
-
-//Calculate page render time
-var renderTime = perfData.domComplete - perfData.domLoading;
-
-/*
-https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
-
-https://developer.mozilla.org/en-US/docs/Web/API/PerformanceTiming/loadEventEnd
-https://developer.mozilla.org/en-US/docs/Web/API/PerformanceNavigationTiming#Browser_compatibility
-https://developer.mozilla.org/en-US/docs/Web/API/Performance/getEntries
-https://developer.mozilla.org/en-US/docs/Web/API/Performance
-*/
--- src/collectors/TimingCollector.spec.js
+++ src/collectors/TimingCollector.spec.js
@@ -1,12 +1,49 @@
+import sinon from 'sinon';
 import TimingCollector from './TimingCollector';
 
+jest.useFakeTimers();
 
 describe('TimingCollector', () => {
+  const originalEvent = {
+    id: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
+    clientId: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc001',
+    eventType: 'state',
+    eventAction: 'load',
+    eventSource: 'products/shoes/1',
+    eventCategory: 'products/shoes',
+    deviceType: 'mobile'
+  };
 
-  test('displays the title', () => {
-    expect(1).toEqual(1);
+  const expectedEvent = {
+    ...originalEvent,
+    eventStart: 123,
+    eventEnd: 456
+  }
+  let timingCollector;
+
+
+  beforeEach(() => {
+    global.window.performance.timing = {
+      navigationStart: 123,
+      domContentLoadedEventEnd: 456
+    }
+
+    timingCollector = new TimingCollector();
   });
 
+  test('Return event with all mandatori fields', async () =>  {
+    const actualEvent = await timingCollector.prepare(originalEvent);
+
+    expect(actualEvent).toEqual(expectedEvent);
+  });
+
+
+  test('Return correct event timing', async () =>  {
+    const actualEvent = await timingCollector.prepare(originalEvent);
+
+    expect(actualEvent.eventStart).toEqual(expectedEvent.eventStart);
+    expect(actualEvent.eventEnd).toEqual(expectedEvent.eventEnd);
+  });
 
 });
 
