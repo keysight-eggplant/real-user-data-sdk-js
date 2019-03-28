ef94c04292348174031b983e46da8df3d6a24e58
Robert Dumitrescu <robert.dumitrescu@nccgroup.trust>
2019-03-28 10:30:14 +0000

    [RUM-18631][RUM-18702][FEATURE] Added dev deps in package.json

---
+++ src/core/Collector.Spec.js
@@ -0,0 +1,19 @@
+'use strict';
+import Collector from "./Collector";
+
+// const expect = require('chai').expect;
+
+describe('Collector', () => {
+
+    let Collector;
+    before(() => {
+
+    });
+
+    describe('-> collect', () => {
+        it('bla', async () => {
+
+            let x = new Collector();
+        });
+    });
+});
--- src/core/Collector.js
+++ src/core/Collector.js
@@ -31,4 +31,4 @@ class Collector {
 	}
 }
 
-export { Collector };
+export default {Collector};
