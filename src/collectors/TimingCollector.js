bda4e266ea320517182d053461cb9d78d1262911
MoHo Khaleqi <habibsandyfan@gmail.com>
2019-03-28 12:00:36 +0000

    [RUM-18631][RUM-18705][FEATURE] working on timing collector

---
+++ src/collectors/TimingCollector.js
@@ -0,0 +1,62 @@
+export default class TimingCollector {
+
+  constructor() {
+    this.eventStart;
+    this.eventEnd;
+  }
+
+   eventStart() {
+    this.eventStart = 123;
+  }
+
+   eventEnd() {
+    this.eventEnd = 456;
+  }
+
+  async prepare (event) {
+    event.eventStart = this.eventStart;
+    event.eventEnd = this.eventEnd;
+
+    return event;
+  }
+
+  // async eventTime(event, timingType) {
+  //   event[timingType] = currenttimestamp;
+  //   return event;
+  // }
+
+
+}
+
+// https://webmasters.stackexchange.com/questions/87764/javascript-page-load-time-calculation-returns-negative
+// https://stackoverflow.com/questions/7606972/measuring-site-load-times-via-performance-api
+/*
+$(window).load(function(){
+  setTimeout(function(){
+  window.performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {};
+  var timing = performance.timing || {};
+  var parseTime = timing.loadEventEnd - timing.responseEnd;
+  console.log('Parsetime: ', parseTime);
+  }, 0);
+ });
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
+//Calculate the total page load time
+var perfData = window.performance.timing;
+var pageLoadTime = perfData.loadEventEnd - perfData.navigationStart;
+
+//Calculate request response time
+var connectTime = perfData.responseEnd - perfData.requestStart;
+
+//Calculate page render time
+var renderTime = perfData.domComplete - perfData.domLoading;
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
+
+https://developer.mozilla.org/en-US/docs/Web/API/PerformanceTiming/loadEventEnd
+https://developer.mozilla.org/en-US/docs/Web/API/PerformanceNavigationTiming#Browser_compatibility
+https://developer.mozilla.org/en-US/docs/Web/API/Performance/getEntries
+https://developer.mozilla.org/en-US/docs/Web/API/Performance
+*/
---
+++ src/collectors/TimingCollector.spec.js
@@ -0,0 +1,13 @@
+import TimingCollector from './TimingCollector';
+
+
+describe('TimingCollector', () => {
+
+  test('displays the title', () => {
+    expect(1).toEqual(1);
+  });
+
+
+});
+
+
