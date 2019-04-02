1e32abfdafa344df9d36185c10ef3c80af31fa78
ash <ashleigh.vincent@eggplant.io>
2019-04-02 17:09:03 +0100

    [RUM-18631][RUM-18711][FEATURE] Session actions

--- src/collectors/StopJourneyActionCollector.spec.js
+++ src/collectors/StopJourneyActionCollector.spec.js
@@ -4,7 +4,8 @@ describe("StopJourneyActionCollector", () => {
   test("test collector sets journeyAction stop on event", async () => {
     let collector = new StopJourneyActionCollector();
 
-    let event = await collector.prepare({});
+    let event = await collector.prepare({id:"123-abc-123"});
     expect(event.journeyAction).toBe("stop");
+    expect(event.id).toBe("123-abc-123");
   });
 });
