671b07e9ae61d7018079214abd3ad567231f7f22
Robert Dumitrescu <robert.dumitrescu@nccgroup.trust>
2019-04-03 11:43:39 +0100

    [RUM-18631][RUM-18702][CHORE] Disabled eslint rule

--- src/core/Collector.js
+++ src/core/Collector.js
@@ -1,20 +1,21 @@
+/* eslint no-await-in-loop: 0 */
 export default class Collector {
-    constructor(transport, collectors) {
-        this.transport = transport;
-        this.collectors = Array.isArray(collectors) ? [].concat(collectors) : [];
+  constructor(transport, collectors) {
+    this.transport = transport;
+    this.collectors = Array.isArray(collectors) ? [].concat(collectors) : [];
+  }
+
+  async collect() {
+    let event = {};
+
+    for (let i = 0; i < this.collectors.length; i++) {
+      event = await this.collectors[i].prepare(event);
+
+      if (!event) {
+        throw new Error(`Invalid event returned by collector ${this.collectors[i].name}`);
+      }
     }
 
-    async collect() {
-        let event = {};
-
-        for (let i = 0; i < this.collectors.length; i++) {
-            event = await this.collectors[i].prepare(event);
-
-            if (!event) {
-                throw new Error("Invalid event returned by collector " + this.collectors[i].name);
-            }
-        }
-
-        await this.transport.execute(event);
-    }
+    await this.transport.execute(event);
+  }
 }
