05112cd898edcd956f7bdaa31178069bd6693d1b
Malcolm Smith <malcolm.smith@nccgroup.trust>
2019-04-05 16:15:50 +0100

    [RUM-18631][RUM-18772][BUG] Full state load

--- examples/Vanilla/Conversion/main.js
+++ examples/Vanilla/Conversion/main.js
@@ -1,16 +1,17 @@
 ((tenancyId, rciSdk) => {
   // Step 1: Create your Checkout collector
   const conversionCollector = new rciSdk.collector.ConversionCollector();
+  const eventAction = new rciSdk.collector.EventActionCollector(rciSdk.EVENT_ACTION.STATE_LOAD_PARTIAL);
 
   // Step 2: Configure your Transport with the tenancyId provided
-  const targetUrl = `https://target.domain/v1/${tenancyId}/stream`;
+  const targetUrl = `https://event.real-user-data.eggplant.cloud/v1/${tenancyId}/stream`;
   const transport = new rciSdk.Transport(targetUrl);
 
   // Step 3: Capture your default collectors
   const defaults = rciSdk.collector.defaultCollectors;
 
   // Step 4: Capture your custom collector
-  const custom = [conversionCollector];
+  const custom = [eventAction, conversionCollector];
 
   // Step 5: Build a new Producer with transport and merged collector
   const producer = new rciSdk.Producer(transport, defaults.concat(custom));
