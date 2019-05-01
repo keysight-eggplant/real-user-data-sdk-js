a4fb1b577cad0384c40d16a8c70d1741db1f13de
Robert Dumitrescu <robert.dumitrescu@nccgroup.trust>
2019-05-01 10:50:58 +0100

    [RUM-18635][RUM-18894][FEATURE] added hardware collector

--- src/collector/HardwareCollector.spec.js
+++ src/collector/HardwareCollector.spec.js
@@ -1,63 +1,81 @@
 /* eslint-disable no-restricted-properties */
 /* eslint-disable no-underscore-dangle */
 
-import DeviceTypeCollector from './DeviceTypeCollector';
+import HardwareCollector from './HardwareCollector';
 
 describe('HardwareCollector', () => {
-    const originalEvent = {
-        id: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
-        clientId: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc001',
-        eventType: 'state',
-        eventAction: 'load',
-        eventSource: 'products/shoes/1',
-        eventCategory: 'products/shoes',
-        eventStart: 123,
-        eventEnd: 456,
-    };
+  const originalEvent = {
+    id: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
+    clientId: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc001',
+    eventType: 'state',
+    eventAction: 'load',
+    eventSource: 'products/shoes/1',
+    eventCategory: 'products/shoes',
+    eventStart: 123,
+    eventEnd: 456,
+  };
 
-    const expectedEvent = {
-        ...originalEvent,
-        deviceType: 'mobile',
-    };
-    let deviceTypeCollector;
-
-
-    describe('Valid UA', () => {
-        beforeEach(() => {
-            const userAgentString = 'Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30';
-
-            navigator.__defineGetter__('userAgent', () => userAgentString);
-
-            deviceTypeCollector = new DeviceTypeCollector();
-        });
-
-        test('Return event with all mandatory fields', async () => {
-            const actualEvent = await deviceTypeCollector.prepare(originalEvent);
-
-            expect(actualEvent).toEqual(expectedEvent);
-        });
+  const expectedEvent = {
+    ...originalEvent,
+    manufacturer: 'Samsung',
+    model: 'SCH-I535',
+    screenResolutionWidth: 0, // this should be modified if we are using Karma
+    screenResolutionHeight: 0 // this should be modified if we are using Karma
+  };
+  let hardwareCollector;
 
 
-        test('Return correct device type', async () => {
-            const actualEvent = await deviceTypeCollector.prepare(originalEvent);
+  describe('Valid UA', () => {
+    beforeEach(() => {
+      const userAgentString = 'Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30';
 
-            expect(actualEvent.deviceType).toEqual(expectedEvent.deviceType);
-        });
+      navigator.__defineGetter__('userAgent', () => userAgentString);
+
+      hardwareCollector = new HardwareCollector();
     });
 
-    describe('Invalid UA', () => {
-        beforeEach(() => {
-            const userAgentString = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36';
+    test('Return event with all mandatory fields', async () => {
+      const actualEvent = await hardwareCollector.prepare(originalEvent);
 
-            navigator.__defineGetter__('userAgent', () => userAgentString);
-
-            deviceTypeCollector = new DeviceTypeCollector();
-        });
-
-        test('Return event with all mandatory fields', async () => {
-            const actualEvent = await deviceTypeCollector.prepare(originalEvent);
-
-            expect(actualEvent.deviceType).toEqual('PC');
-        });
+      expect(actualEvent).toEqual(expectedEvent);
     });
+
+
+    test('Return correct device type', async () => {
+      const actualEvent = await hardwareCollector.prepare(originalEvent);
+
+      expect(actualEvent.deviceType).toEqual(expectedEvent.deviceType);
+    });
+  });
+
+  describe('Invalid UA', () => {
+    beforeEach(() => {
+      const userAgentString = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36';
+
+      navigator.__defineGetter__('userAgent', () => userAgentString);
+
+      hardwareCollector = new HardwareCollector();
+    });
+
+    test('Return event with default manufacturer', async () => {
+      const actualEvent = await hardwareCollector.prepare(originalEvent);
+
+      expect(actualEvent.manufacturer).toEqual('');
+    });
+    test('Return event with default model', async () => {
+      const actualEvent = await hardwareCollector.prepare(originalEvent);
+
+      expect(actualEvent.model).toEqual('');
+    });
+    test('Return event with default screenResolutionWidth', async () => {
+      const actualEvent = await hardwareCollector.prepare(originalEvent);
+
+      expect(actualEvent.screenResolutionWidth).toEqual(0);
+    });
+    test('Return event with default screenResolutionHeight', async () => {
+      const actualEvent = await hardwareCollector.prepare(originalEvent);
+
+      expect(actualEvent.screenResolutionHeight).toEqual(0);
+    });
+  });
 });
