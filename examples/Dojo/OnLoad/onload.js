01f0693808d09a7d240b778dce759994bc7b4787
Malcolm Smith <malcolm.smith@nccgroup.trust>
2020-03-19 17:43:27 +0000

    [RUM-20982][RUM-21042][DOCS] Example Dojo router

--- examples/Dojo/OnLoad/onload.js
+++ examples/Dojo/OnLoad/onload.js
@@ -1,55 +1,55 @@
-const rciSdkProducerFactory = (customCollectors) => {
-    const tenancyId = '123';
-    const targetUrl = `https://target.domain/v1/${tenancyId}/stream`;
-    const transport = new rciSdk.Transport(targetUrl);
-    const defaults = rciSdk.collector.defaultCollectors;
-    const collection = defaults.concat(customCollectors);
-    return new rciSdk.Producer(transport, collection);
-};
+function rciSdkProducerFactory(customCollectors) {
+  var tenancyId = '123';
+  var targetUrl = `https://target.domain/v1/${tenancyId}/stream`;
+  var transport = new rciSdk.Transport(targetUrl);
+  var defaults = rciSdk.collector.defaultCollectors;
+  var collection = defaults.concat(customCollectors);
+  return new rciSdk.Producer(transport, collection);
+}
 
 require(["dojo/router", "dojo/dom", "dojo/on"],
-    function(router, dom, on){
-        router.register("/foo/bar/1", function(evt){
-            evt.preventDefault();
+    function (router, dom, on) {
+      router.register("/foo/bar/1", function (evt) {
+        evt.preventDefault();
 
-            const customCollector = {
-                prepare: async (event) => {
-                    event.eventStart = new Date().getTime();
-                    event.eventEnd = new Date().getTime();
-                    event.eventAction = 'dojo_router';
-                    event.eventCategory = evt.newPath;
-                    return event;
-                }
-            };
+        var customCollector = {
+          prepare: function (event) {
+            event.eventStart = new Date().getTime();
+            event.eventEnd = new Date().getTime();
+            event.eventAction = 'dojo_router';
+            event.eventCategory = evt.newPath;
+            return event;
+          }
+        };
 
-            // Step 4: Call your factory
-            rciSdkProducerFactory([customCollector]).collect();
-        });
+        // Step 4: Call your factory
+        rciSdkProducerFactory([customCollector]).collect();
+      });
 
-        router.register("/foo/bar/2", function(evt){
-            evt.preventDefault();
+      router.register("/foo/bar/2", function (evt) {
+        evt.preventDefault();
 
-            const customCollector = {
-                prepare: async (event) => {
-                    event.eventStart = new Date().getTime();
-                    event.eventEnd = new Date().getTime();
-                    event.eventAction = 'dojo_router';
-                    event.eventCategory = evt.newPath;
-                    return event;
-                }
-            };
+        var customCollector = {
+          prepare: function (event) {
+            event.eventStart = new Date().getTime();
+            event.eventEnd = new Date().getTime();
+            event.eventAction = 'dojo_router';
+            event.eventCategory = evt.newPath;
+            return event;
+          }
+        };
 
-            // Step 4: Call your factory
-            rciSdkProducerFactory([customCollector]).collect();
-        });
+        // Step 4: Call your factory
+        rciSdkProducerFactory([customCollector]).collect();
+      });
 
-        router.startup();
+      router.startup();
 
-        on(dom.byId("changeHash1"), "click", function(){
-            router.go("/foo/bar/1");
-        });
+      on(dom.byId("changeHash1"), "click", function () {
+        router.go("/foo/bar/1");
+      });
 
-        on(dom.byId("changeHash2"), "click", function(){
-            router.go("/foo/bar/2");
-        });
+      on(dom.byId("changeHash2"), "click", function () {
+        router.go("/foo/bar/2");
+      });
     });
