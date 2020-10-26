f57a0c9c79707468d0ef16eba3519eeb9051549a
Robert Dumitrescu <robertdum01@gmail.com>
2020-10-26 12:54:37 +0000

    fixed eslint

--- src/collector/InstrumentationVersionCollector.spec.js
+++ src/collector/InstrumentationVersionCollector.spec.js
@@ -3,115 +3,115 @@ import InstrumentationVersionCollector from './InstrumentationVersionCollector';
 const coreVersion = 'v11.2.9';
 
 jest.mock('../core/version.constant', () => ({
-    __esModule: true, // this property makes it work
-    default: 'v11.2.9'
+  __esModule: true, // this property makes it work
+  default: 'v11.2.9'
 }));
 
 describe('InstrumentationVersionCollector', () => {
-    const originalEvent = {
-        id: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
-        clientId: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc001',
-        eventType: 'state',
-        eventAction: 'load',
-        eventSource: 'products/shoes/1',
-        eventCategory: 'products/shoes',
-        deviceType: 'mobile'
-    };
+  const originalEvent = {
+    id: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
+    clientId: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc001',
+    eventType: 'state',
+    eventAction: 'load',
+    eventSource: 'products/shoes/1',
+    eventCategory: 'products/shoes',
+    deviceType: 'mobile'
+  };
 
-    const instrumentationVersionHash = '87a97607d305fab320c8837e4f802bc89b8d52b6';
-    const instrumentationVersion = '11.2.9';
+  const instrumentationVersionHash = '87a97607d305fab320c8837e4f802bc89b8d52b6';
+  const instrumentationVersion = '11.2.9';
 
-    /** @type {Event} */
-    const expectedEventWithCore = {
-        ...originalEvent,
-        softwareInfo5: coreVersion
-    };
+  /** @type {Event} */
+  const expectedEventWithCore = {
+    ...originalEvent,
+    softwareInfo5: coreVersion
+  };
 
-    /** @type {Event} */
-    const expectedEventWithCoreAndInstrumentationAsHash = {
-        ...originalEvent,
-        softwareInfo5: `${coreVersion}_87a97607`
-    };
+  /** @type {Event} */
+  const expectedEventWithCoreAndInstrumentationAsHash = {
+    ...originalEvent,
+    softwareInfo5: `${coreVersion}_87a97607`
+  };
 
-    /** @type {Event} */
-    const expectedEventWithCoreAndInstrumentationAsVersion = {
-        ...originalEvent,
-        softwareInfo5: `${coreVersion}_${instrumentationVersion}`
-    };
-    let instrumentationVersionCollector;
+  /** @type {Event} */
+  const expectedEventWithCoreAndInstrumentationAsVersion = {
+    ...originalEvent,
+    softwareInfo5: `${coreVersion}_${instrumentationVersion}`
+  };
+  let instrumentationVersionCollector;
 
-    describe('with RCI SDK corrupt', () => {
-        beforeEach(() => {
-            instrumentationVersionCollector = new InstrumentationVersionCollector();
-        });
-
-        test('Return event with all mandatory fields', async () => {
-            /** @type {Event} */
-            const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
-
-            expect(actualEvent).toEqual(originalEvent);
-        });
+  describe('with RCI SDK corrupt', () => {
+    beforeEach(() => {
+      instrumentationVersionCollector = new InstrumentationVersionCollector();
     });
 
-    describe('with no instrumentation version passed', () => {
-        beforeEach(() => {
-            instrumentationVersionCollector = new InstrumentationVersionCollector();
-        });
+    test('Return event with all mandatory fields', async () => {
+      /** @type {Event} */
+      const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
 
-        test('Return event with all mandatory fields', async () => {
-            /** @type {Event} */
-            const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
+      expect(actualEvent).toEqual(originalEvent);
+    });
+  });
 
-            expect(actualEvent).toEqual(expectedEventWithCore);
-        });
-
-        test('Return event with the core version if an instrumentation version is not passed', async () => {
-            /** @type {Event} */
-            const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
-
-            expect(actualEvent.softwareInfo5).toEqual(coreVersion);
-        });
+  describe('with no instrumentation version passed', () => {
+    beforeEach(() => {
+      instrumentationVersionCollector = new InstrumentationVersionCollector();
     });
 
-    describe('with no instrumentation version passed as hash', () => {
-        beforeEach(() => {
-            instrumentationVersionCollector = new InstrumentationVersionCollector(instrumentationVersionHash);
-        });
+    test('Return event with all mandatory fields', async () => {
+      /** @type {Event} */
+      const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
 
-        test('Return event with all mandatory fields', async () => {
-            /** @type {Event} */
-            const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
-
-            expect(actualEvent).toEqual(expectedEventWithCoreAndInstrumentationAsHash);
-        });
-
-        test('Return event with the core version if an instrumentation version is passed as hash', async () => {
-            /** @type {Event} */
-            const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
-
-            expect(actualEvent.softwareInfo5).toEqual(expectedEventWithCoreAndInstrumentationAsHash.softwareInfo5);
-        });
+      expect(actualEvent).toEqual(expectedEventWithCore);
     });
 
-    describe('with no instrumentation version passed as version', () => {
-        beforeEach(() => {
-            instrumentationVersionCollector = new InstrumentationVersionCollector(instrumentationVersion);
-        });
+    test('Return event with the core version if an instrumentation version is not passed', async () => {
+      /** @type {Event} */
+      const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
 
-        test('Return event with all mandatory fields', async () => {
-            /** @type {Event} */
-            const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
-
-            expect(actualEvent).toEqual(expectedEventWithCoreAndInstrumentationAsVersion);
-        });
-
-        test('Return event with the core version if an instrumentation version is passed as version', async () => {
-            /** @type {Event} */
-            const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
-
-            expect(actualEvent.softwareInfo5).toEqual(expectedEventWithCoreAndInstrumentationAsVersion.softwareInfo5);
-        });
+      expect(actualEvent.softwareInfo5).toEqual(coreVersion);
     });
+  });
+
+  describe('with no instrumentation version passed as hash', () => {
+    beforeEach(() => {
+      instrumentationVersionCollector = new InstrumentationVersionCollector(instrumentationVersionHash);
+    });
+
+    test('Return event with all mandatory fields', async () => {
+      /** @type {Event} */
+      const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
+
+      expect(actualEvent).toEqual(expectedEventWithCoreAndInstrumentationAsHash);
+    });
+
+    test('Return event with the core version if an instrumentation version is passed as hash', async () => {
+      /** @type {Event} */
+      const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
+
+      expect(actualEvent.softwareInfo5).toEqual(expectedEventWithCoreAndInstrumentationAsHash.softwareInfo5);
+    });
+  });
+
+  describe('with no instrumentation version passed as version', () => {
+    beforeEach(() => {
+      instrumentationVersionCollector = new InstrumentationVersionCollector(instrumentationVersion);
+    });
+
+    test('Return event with all mandatory fields', async () => {
+      /** @type {Event} */
+      const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
+
+      expect(actualEvent).toEqual(expectedEventWithCoreAndInstrumentationAsVersion);
+    });
+
+    test('Return event with the core version if an instrumentation version is passed as version', async () => {
+      /** @type {Event} */
+      const actualEvent = await instrumentationVersionCollector.prepare(originalEvent);
+
+      expect(actualEvent.softwareInfo5).toEqual(expectedEventWithCoreAndInstrumentationAsVersion.softwareInfo5);
+    });
+  });
 
 
 });
--- src/core/Normalization.helper.spec.js
+++ src/core/Normalization.helper.spec.js
@@ -2,134 +2,134 @@ import NormalizationHelper from './Normalization.helper';
 
 describe('NormalizationHelper Unit Tests', () => {
 
-    it('should normalize an empty string', async () => {
+  it('should normalize an empty string', async () => {
 
-        const initial = '';
+    const initial = '';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial);
-        const expected = null;
+    const actual = NormalizationHelper.normalizeGoalValue(initial);
+    const expected = null;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is an integer', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is an integer', async () => {
 
-        const initial = 948;
+    const initial = 948;
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial);
-        const expected = 94800;
+    const actual = NormalizationHelper.normalizeGoalValue(initial);
+    const expected = 94800;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is a float', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is a float', async () => {
 
-        const initial = 948.342;
+    const initial = 948.342;
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial);
-        const expected = 94834;
+    const actual = NormalizationHelper.normalizeGoalValue(initial);
+    const expected = 94834;
 
-        expect(actual).toEqual(expected);
-    });
+    expect(actual).toEqual(expected);
+  });
 
-    it('should normalize when is a float (one decimal)', async () => {
+  it('should normalize when is a float (one decimal)', async () => {
 
-        const initial = 948.3;
+    const initial = 948.3;
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial);
-        const expected = 94830;
+    const actual = NormalizationHelper.normalizeGoalValue(initial);
+    const expected = 94830;
 
-        expect(actual).toEqual(expected);
-    });
+    expect(actual).toEqual(expected);
+  });
 
-    it('should normalize when is an integer (as string)', async () => {
+  it('should normalize when is an integer (as string)', async () => {
 
-        const initial = '948';
+    const initial = '948';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial);
-        const expected = 94800;
+    const actual = NormalizationHelper.normalizeGoalValue(initial);
+    const expected = 94800;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is a float (as string)', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is a float (as string)', async () => {
 
-        const initial = '948.342';
+    const initial = '948.342';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial);
-        const expected = 94834;
+    const actual = NormalizationHelper.normalizeGoalValue(initial);
+    const expected = 94834;
 
-        expect(actual).toEqual(expected);
-    });
+    expect(actual).toEqual(expected);
+  });
 
-    it('should normalize when is a float (one decimal) (as string)', async () => {
+  it('should normalize when is a float (one decimal) (as string)', async () => {
 
-        const initial = '948.3';
+    const initial = '948.3';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial);
-        const expected = 94830;
+    const actual = NormalizationHelper.normalizeGoalValue(initial);
+    const expected = 94830;
 
-        expect(actual).toEqual(expected);
-    });
+    expect(actual).toEqual(expected);
+  });
 
-    it('should normalize an empty string with fullUnit on false', async () => {
+  it('should normalize an empty string with fullUnit on false', async () => {
 
-        const initial = '';
+    const initial = '';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial, false);
-        const expected = null;
+    const actual = NormalizationHelper.normalizeGoalValue(initial, false);
+    const expected = null;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is an integer with fullUnit on false (as string)', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is an integer with fullUnit on false (as string)', async () => {
 
-        const initial = '948';
+    const initial = '948';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial, false);
-        const expected = 948;
+    const actual = NormalizationHelper.normalizeGoalValue(initial, false);
+    const expected = 948;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is a float with fullUnit on false (as string)', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is a float with fullUnit on false (as string)', async () => {
 
-        const initial = '948.342';
+    const initial = '948.342';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial, false);
-        const expected = 948;
+    const actual = NormalizationHelper.normalizeGoalValue(initial, false);
+    const expected = 948;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is a float (one decimal) with fullUnit on false (as string)', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is a float (one decimal) with fullUnit on false (as string)', async () => {
 
-        const initial = '948.3';
+    const initial = '948.3';
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial, false);
-        const expected = 948;
+    const actual = NormalizationHelper.normalizeGoalValue(initial, false);
+    const expected = 948;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is an integer with fullUnit on false', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is an integer with fullUnit on false', async () => {
 
-        const initial = 948;
+    const initial = 948;
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial, false);
-        const expected = 948;
+    const actual = NormalizationHelper.normalizeGoalValue(initial, false);
+    const expected = 948;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is a float with fullUnit on false', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is a float with fullUnit on false', async () => {
 
-        const initial = 948.342;
+    const initial = 948.342;
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial, false);
-        const expected = 948;
+    const actual = NormalizationHelper.normalizeGoalValue(initial, false);
+    const expected = 948;
 
-        expect(actual).toEqual(expected);
-    });
-    it('should normalize when is a float (one decimal) with fullUnit on false', async () => {
+    expect(actual).toEqual(expected);
+  });
+  it('should normalize when is a float (one decimal) with fullUnit on false', async () => {
 
-        const initial = 948.3;
+    const initial = 948.3;
 
-        const actual = NormalizationHelper.normalizeGoalValue(initial, false);
-        const expected = 948;
+    const actual = NormalizationHelper.normalizeGoalValue(initial, false);
+    const expected = 948;
 
-        expect(actual).toEqual(expected);
-    });
+    expect(actual).toEqual(expected);
+  });
 });
