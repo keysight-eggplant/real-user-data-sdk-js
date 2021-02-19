246956a97ceb0b10bf7ece7d3316841c59c509bb
Malcolm Smith <malcolm.smith@nccgroup.trust>
2021-02-19 18:09:06 +0000

    [RUM-22934][RUM-23164][REFACTOR] Composition with pre-defined

--- examples/Vanilla/WebBasedCollectorsCollection/webBasedCollectorsCollection.js
+++ examples/Vanilla/WebBasedCollectorsCollection/webBasedCollectorsCollection.js
@@ -6,14 +6,18 @@
   // Step 2.a: Capture your default collectors
   const defaults = rciSdk.collector.defaultCollectors;
 
-  // Step 2.b: Capture WebFocusedCollectors
-  const {webFocusedCollectors} = rciSdk.collector;
+  // Step 2.c: Create the pre-defined collectors collection
+  const customPreDefined = [
+    ...rciSdk.collector.webFocusedCollectors,
+    new rciSdk.collector.InstrumentationVersionCollector('v1.2.3')
+  ];
 
-  // Step 2.c: Create the final collectors collection
-  const finalCollectorCollection = defaults.concat(webFocusedCollectors);
+  // Step 2.d: Create the custom collectors collection
+  const custom = [
+  ];
 
-  // Step 3: Build a new Producer with transport and collector
-  const producer = new rciSdk.Producer(transport, finalCollectorCollection);
+  // Step 3: Build a new Producer with transport and collectors
+  const producer = new rciSdk.Producer(transport, defaults.concat(customPreDefined, custom));
 
   // Step 4: Register your hook
   // Caution: There may already be an onload registered - in which case use a decorator pattern.
--- src/collector/webFocusedCollectors.js
+++ src/collector/webFocusedCollectors.js
@@ -2,11 +2,14 @@ import WebBackEndCollector from './WebBackEndCollector';
 import WebPageLoadTimesCollector from './WebPageLoadTimesCollector';
 import WebPaintTimesCollector from './WebPaintTimesCollector';
 import WebVitalsCollector from './WebVitalsCollector';
+import PerformanceServiceFactory from '../core/performance/PerformanceServiceFactory';
+
+const performanceServiceFactory = new PerformanceServiceFactory();
 
 /** Collection for web focused collectors. Those are not necessary in non-web applications */
 export default [
-  new WebBackEndCollector(),
-  new WebPageLoadTimesCollector(),
-  new WebPaintTimesCollector(),
+  new WebBackEndCollector(performanceServiceFactory),
+  new WebPageLoadTimesCollector(performanceServiceFactory),
+  new WebPaintTimesCollector(performanceServiceFactory),
   new WebVitalsCollector()
 ];
